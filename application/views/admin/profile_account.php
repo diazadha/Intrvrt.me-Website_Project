df6ae112e7fde3b4a384f1080b20dcff480cb12b
rinaldimahendra <90098741+rinaldimahendra@users.noreply.github.com>
2021-09-06 12:36:53 +0700

    update profile account admin

--- application/views/admin/profile_account.php
+++ application/views/admin/profile_account.php
@@ -5,13 +5,13 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Register Admin</h1>
+            <h1 class="m-0 text-dark">Profile Admin</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">HOME</a></li>
               <li class="breadcrumb-item active">USERS</li>
-              <li class="breadcrumb-item active">REGISTER ADMIN</li>
+              <li class="breadcrumb-item active">EDIT PROFILE</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -27,7 +27,7 @@
             <div class="card card-outline card-primary">
                 <div class="card-header">
                 <h3 class="card-title">
-                    Form Regist
+                    Edit Profile Admin
                 </h3>
                 </div>
                 <form class="form-horizontal">
