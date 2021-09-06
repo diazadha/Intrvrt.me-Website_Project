ac78ba76ed673a23d42b0195b06ce5bb19532880
rinaldimahendra <90098741+rinaldimahendra@users.noreply.github.com>
2021-09-06 12:26:45 +0700

    update register admin

--- application/views/admin/register_admin.php
+++ application/views/admin/register_admin.php
@@ -1,56 +1,90 @@
-<div class="container-fluid layout-fixed">
-    <div></div>
-    <div class="row align-items-center">
-        <div class="col-sm-6">
-            <h1 class="h3 mb-2 text-gray-800">Register Admin</h1>
-        </div>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Register Admin</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">HOME</a></li>
+              <li class="breadcrumb-item active">USERS</li>
+              <li class="breadcrumb-item active">REGISTER ADMIN</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content-header -->
 
-    <div class="card shadow mb-4">
-        
-        <div class="card-body"><br>
-            <form action="#" method="post" enctype="multipart/form-data">
-                
-                <div class="row justify-content-center">
-                    <div class="col-6">
-
-                        <h5 class="text-center mb-4"><b>Pembuatan Akun</b></h5>
-                        <label for="basic-url" class="form-label"><b>Nama Admin:</b></label>
-                        <div class="input-group mb-4">
-                            <input type="text" name="nama" class="form-control" placeholder="Masukkan Nama Lengkap" aria-label="Masukkan Nama Lengkap" aria-describedby="basic-addon1">
-                        </div>
-
-                        <label for="basic-url" class="form-label"><b>Email:</b></label>
-                        <div class="input-group mb-4">
-                            <input type="text" name="email" class="form-control" placeholder="Masukkan email" aria-label="Masukkan email" aria-describedby="basic-addon1">
-                        </div>
-
-                        <label for="basic-url" class="form-label"><b>Password:</b></label>
-                        <div class="input-group mb-4">
-                            <input type="password" name="password" class="form-control" placeholder="Masukkan password" aria-label="Masukkan password" aria-describedby="basic-addon1">
-                        </div>
-
-                        <label for="basic-url" class="form-label"><b>Jenis Kelamin:</b></label><br>
-                        <div class="form-check form-check-inline ml-2 mb-4">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                            <label class="form-check-label" for="inlineRadio1">Laki-laki</label>
-                        </div>
-                        <div class="form-check form-check-inline mb-4">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                            <label class="form-check-label" for="inlineRadio2">Perempuan</label>
-                        </div><br>
-
-                        <label for="basic-url" class="form-label"><b>Tanggal Lahir:</b></label>
-                        <div class="input-group mb-4">
-                            <input type="date" name="tanggal" class="form-control" placeholder="Masukkan password" aria-label="Masukkan password" aria-describedby="basic-addon1">
-                        </div>
-
-                    </div>
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+            <div class="card card-outline card-primary">
+                <div class="card-header">
+                <h3 class="card-title">
+                    Form Regist
+                </h3>
                 </div>
+                <form class="form-horizontal">
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-2 col-form-label">Nama</label>
+                            <div class="col-sm-10">
+                                <input type="nama" class="form-control" id="nama" placeholder="Nama Lengkap" value=>
+                            </div>
+                        </div>
 
-                <button type="submit" class="btn btn-danger btn-block" >Daftarkan</button><br>
-            
-            </form>
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" placeholder="Email" value=>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="password" placeholder="Password" value=>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="date" class="col-sm-2 col-form-label">Jenis Kelamin</label>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline ml-2 mb-4">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                    <label class="form-check-label" for="inlineRadio1">Laki-laki</label>
+                                </div>
+                                <div class="form-check form-check-inline mb-4">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                    <label class="form-check-label" for="inlineRadio2">Perempuan</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="date" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                            <div class="col-sm-2">
+                                <input type="date" class="form-control" id="date" placeholder="Tanggal Lahir" value=>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-info">Regist</button>
+                    </div>
+                    <!-- /.card-footer -->
+                </form>
+            </div>
+            </div>
+            <!-- /.col-->
         </div>
-    </div>
-</div>   
+        <!-- ./row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
