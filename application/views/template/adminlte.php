9b69bfd53e3576963b4fa059d38fa66b9c44915b
Makmudin <34791571+MakmudinID@users.noreply.github.com>
2021-09-07 09:46:53 +0700

    Update hight summernote editor

--- application/views/template/adminlte.php
+++ application/views/template/adminlte.php
@@ -284,7 +284,7 @@
   $(function () {
     // Summernote
     $('.textarea').summernote({
-        height: 650  //set editable area's height
+        height: 250  //set editable area's height
     })
   })
 </script>
