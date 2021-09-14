9d07fbf587bf086c56ffec27b80ad35248832020
rinaldimahendra <90098741+rinaldimahendra@users.noreply.github.com>
2021-09-14 15:46:50 +0700

    update CRUD

--- application/models/tiket_model.php
+++ application/models/tiket_model.php
@@ -27,4 +27,54 @@ class tiket_model extends CI_Model
 
         return $this->db->query($query);
     }
+	
+	public function delete_event($id)
+    {
+        $query = "DELETE FROM event
+        WHERE id_event = $id";
+
+        return $this->db->query($query);
+    }
+
+    public function getdataevent(){
+        $query = "SELECT *
+        FROM event
+        ";
+
+        return $this->db->query($query);
+    }
+
+    public function tambah_event($foto = NULL){
+        $data=array(
+            'nama_event' => htmlspecialchars($this->security->xss_clean($this->input->post('nama_event')),ENT_QUOTES),
+            'kategori' => htmlspecialchars($this->security->xss_clean($this->input->post('kategori')),ENT_QUOTES),
+            'foto' => htmlspecialchars($foto, ENT_QUOTES),
+            'harga_tiket' => htmlspecialchars($this->security->xss_clean($this->input->post('harga')),ENT_QUOTES),
+            'tgl_aktif' => htmlspecialchars($this->security->xss_clean($this->input->post('tgl_aktif')),ENT_QUOTES),
+            'tgl_berakhir' => htmlspecialchars($this->security->xss_clean($this->input->post('tgl_berakhir')),ENT_QUOTES),
+            'deskripsi_event' => htmlspecialchars($this->security->xss_clean($this->input->post('deskripsi')),ENT_QUOTES),
+        );
+        return $this->db->insert('event',$data);
+    }
+
+    public function update_foto($foto = NULL){
+        $data=array(
+        'foto' => htmlspecialchars($foto, ENT_QUOTES),
+        );
+        $this->db->where('id_event', htmlspecialchars($this->security->xss_clean($this->input->post('id_event')),ENT_QUOTES));
+        return $this->db->update('event', $data);
+    }
+
+    public function update_event(){
+        $data=array(
+            'nama_event' => htmlspecialchars($this->security->xss_clean($this->input->post('nama_event')),ENT_QUOTES),
+            'kategori' => htmlspecialchars($this->security->xss_clean($this->input->post('kategori')),ENT_QUOTES),
+            'harga_tiket' => htmlspecialchars($this->security->xss_clean($this->input->post('harga_tiket')),ENT_QUOTES),
+            'tgl_aktif' => htmlspecialchars($this->security->xss_clean($this->input->post('tgl_aktif')),ENT_QUOTES),
+            'tgl_berakhir' => htmlspecialchars($this->security->xss_clean($this->input->post('tgl_berakhir')),ENT_QUOTES),
+            'deskripsi_event' => htmlspecialchars($this->security->xss_clean($this->input->post('deskripsi')),ENT_QUOTES),
+        );
+        $this->db->where('id_event', htmlspecialchars($this->security->xss_clean($this->input->post('id_event')),ENT_QUOTES));
+        return $this->db->update('event', $data);
+    }
 }
