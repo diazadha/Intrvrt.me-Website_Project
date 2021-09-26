55278c4d524bb4a466bee3a2c84670cbd346110a
Diaz Adha Asri Prakoso <76459391+diazadha@users.noreply.github.com>
2021-09-26 16:02:23 +0700

    fix auth validate in partner

--- application/controllers/admin/Partner.php
+++ application/controllers/admin/Partner.php
@@ -5,28 +5,39 @@ class Partner extends CI_Controller
 {
     public function __construct()
     {
-      parent::__construct();
-      $this->load->model('PartnerModel');
+        parent::__construct();
+        $this->load->model('PartnerModel');
     }
 
-    public function index(){
-        $data['title'] = 'Partner';
-        $data['content'] = "admin/partner";
-        $data['js'] = array("partner.js?r=".rand());
-		$this->load->view("template/adminlte", $data);
+    public function index()
+    {
+        $user = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        if ($user) {
+            if ($user['id_role'] == 2) {
+                redirect('home');
+            } else {
+                $data['title'] = 'Partner';
+                $data['content'] = "admin/partner";
+                $data['js'] = array("partner.js?r=" . rand());
+                $this->load->view("template/adminlte", $data);
+            }
+        } else {
+            redirect('home');
+        }
     }
 
-    
-    public function partner_(){
+
+    public function partner_()
+    {
         $list = $this->PartnerModel->partner_data();
         $data = array();
         foreach ($list as $field) {
             $row = array();
             $row[] = $field->nama;
-            $row[] = '<img src="'.$field->foto.'" class="img-thumbnail" width="200">';
+            $row[] = '<img src="' . $field->foto . '" class="img-thumbnail" width="200">';
             $row[] = ($field->status == 1) ? '<span class="badge badge-success">On</span>' : '<span class="badge badge-danger">Off</span>';
-            $row[] = '<button type="button" class="btn btn-info btn-sm edit" data-id="'.$field->id_partner.'" data-foto="'.$field->foto.'" data-nama="'.$field->nama.'" data-status="'.$field->status.'">Edit</button>
-                      <button type="button" class="ml-1 btn btn-danger delete btn-sm" data-id="'.$field->id_partner.'" data-nama="'.$field->nama.'">Hapus</button>';
+            $row[] = '<button type="button" class="btn btn-info btn-sm edit" data-id="' . $field->id_partner . '" data-foto="' . $field->foto . '" data-nama="' . $field->nama . '" data-status="' . $field->status . '">Edit</button>
+                      <button type="button" class="ml-1 btn btn-danger delete btn-sm" data-id="' . $field->id_partner . '" data-nama="' . $field->nama . '">Hapus</button>';
             $data[] = $row;
         }
         $output = array(
@@ -39,81 +50,72 @@ class Partner extends CI_Controller
     }
 
     public function create_partner()
-	{
-		if($_FILES["foto"]["name"])
-		{  
-			$config['allowed_types'] = 'jpg|png|jpeg';
+    {
+        if ($_FILES["foto"]["name"]) {
+            $config['allowed_types'] = 'jpg|png|jpeg';
             $config['max_size']  = '2024';
             $config['upload_path'] = './assets/partner';
             $config['encrypt_name'] = TRUE;
             $this->load->library('upload', $config);
             $this->upload->initialize($config);
-			if(!$this->upload->do_upload('foto'))  
-			{   
-				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+            if (!$this->upload->do_upload('foto')) {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
                 redirect('admin/partner/index');
-			}  
-			else  
-			{  
-				$fileupload = $this->upload->data();
+            } else {
+                $fileupload = $this->upload->data();
                 $filename = pathinfo($fileupload['full_path']);
-                $foto = base_url('assets/partner/'.$filename['basename']); 
-			}  
-		}else{
-			$foto = null; 
-		}  
+                $foto = base_url('assets/partner/' . $filename['basename']);
+            }
+        } else {
+            $foto = null;
+        }
 
-		if(!$this->PartnerModel->create_partner($foto)){
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gagal menambahkan partner</div>');
-        	redirect('admin/partner/index');
-		}
-	}
+        if (!$this->PartnerModel->create_partner($foto)) {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gagal menambahkan partner</div>');
+            redirect('admin/partner/index');
+        }
+    }
 
-	public function update_partner()
-	{
-		if($_FILES["foto"]["name"])  
-		{  
-			$config['allowed_types'] = 'jpg|png|jpeg';
+    public function update_partner()
+    {
+        if ($_FILES["foto"]["name"]) {
+            $config['allowed_types'] = 'jpg|png|jpeg';
             $config['max_size']  = '2024';
             $config['upload_path'] = './assets/partner';
             $config['encrypt_name'] = TRUE;
             $this->load->library('upload', $config);
             $this->upload->initialize($config);
-			if(!$this->upload->do_upload('foto'))  
-			{   
-				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+            if (!$this->upload->do_upload('foto')) {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
                 redirect('admin/partner/index');
-			}  
-			else  
-			{  
-				$fileupload = $this->upload->data();
+            } else {
+                $fileupload = $this->upload->data();
                 $filename = pathinfo($fileupload['full_path']);
-                $foto = base_url('assets/partner/'.$filename['basename']);
-			}  
-		}else{
-			$foto = $this->input->post('foto_'); 
-			$is_unlink = 'no';
-		} 
+                $foto = base_url('assets/partner/' . $filename['basename']);
+            }
+        } else {
+            $foto = $this->input->post('foto_');
+            $is_unlink = 'no';
+        }
 
-		if(!$this->PartnerModel->update_partner($foto, $is_unlink)){
-			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gagal update data partner</div>');
-        	redirect('admin/partner/index');
-		}
-	}
+        if (!$this->PartnerModel->update_partner($foto, $is_unlink)) {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gagal update data partner</div>');
+            redirect('admin/partner/index');
+        }
+    }
 
-	function delete_partner($id){ 
-        $id_ = htmlspecialchars($id,ENT_QUOTES);
-		if($this->PartnerModel->delete_partner($id_)){
+    function delete_partner($id)
+    {
+        $id_ = htmlspecialchars($id, ENT_QUOTES);
+        if ($this->PartnerModel->delete_partner($id_)) {
             $r['title'] = 'Sukses!';
             $r['icon'] = 'success';
             $r['status'] = 'Berhasil di Hapus!';
-		}else{
+        } else {
             $r['title'] = 'Maaf!';
             $r['icon'] = 'error';
             $r['status'] = '<br><b>Tidak dapat di Hapus! <br> Silakan hubungi Administrator.</b>';
-		}
-		echo json_encode($r);
-	}
-
-
+        }
+        echo json_encode($r);
+    }
 }
