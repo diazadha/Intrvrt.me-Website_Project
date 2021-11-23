2b74e59d3e0cf8b9bed5c34dd09e4524001a2f30
Diaz Adha Asri Prakoso <76459391+diazadha@users.noreply.github.com>
2021-11-23 23:55:55 +0700

    style(event): change layout detail event

--- application/views/detail_event.php
+++ application/views/detail_event.php
@@ -37,93 +37,75 @@
                          </div>
                          </center>
                      </div>
-                     <div class="row">
-                         <div class="col-2">
-                             <h6>Tanggal Acara</h6>
-                         </div>
-                         <div class="col entry-meta meta-0 font-small mb-30 mt-1">
-                             <span class="post-cat bg-danger color-white"><?= $getdatabyid['tgl_acara']  ?></span>
-                         </div>
-                     </div>
-                     <div class="row">
-                         <div class="col-2">
-                             <h6>Batas Beli</h6>
-                         </div>
-                         <div class="col entry-meta meta-0 font-small mb-30 mt-1">
-                             <span class="post-cat bg-danger color-white"><?= $getdatabyid['tgl_berakhir']  ?></span>
-                         </div>
-                     </div>
-                     <div class="row">
-                         <div class="col-2">
-                             <h6>Harga Tiket</h6>
-                         </div>
-                         <div class="col">
-                             <?php
-                                if ($getdatabyid['harga_tiket'] == 0) { ?>
-                                 <a class="entry-meta meta-0"><span class="post-in background5 text-dark font-x-small">GRATIS</span></a>
-                             <?php } else if ($getdatabyid['diskon'] != 0) { ?>
-                                 <?php $h_diskon = $getdatabyid['harga_tiket'] * ($getdatabyid['diskon'] / 100);
-                                    $diskon = $getdatabyid['harga_tiket'] - $h_diskon;
-                                    ?>
-                                 <div class="entry-meta meta-0 font-small mb-30 mt-1">
-                                     <span class="post-cat bg-success color-white" style="text-decoration: line-through;">Rp <?= number_format($getdatabyid['harga_tiket'], 0, ',', '.') ?></span>
-                                     <span class="post-in background5 text-dark font-x-small">Rp <?= number_format($diskon, 0, ',', '.') ?></span>
-                                 </div>
-                             <?php } else { ?>
-                                 <div class="entry-meta meta-0 font-small mb-30 mt-1">
-                                     <span class="post-cat bg-success color-white">Rp <?= number_format($getdatabyid['harga_tiket'], 0, ',', '.') ?></span>
-                                 </div>
-                             <?php } ?>
-                         </div>
-                     </div>
-                     <?php if ($getdatabyid['diskon'] != 0) : ?>
-                         <div class="row">
-                             <div class="col-2">
-                                 <h6>Diskon</h6>
-                             </div>
-                             <div class="col">
-                                 <div class="entry-meta meta-0 font-small mb-30 mt-1">
-                                     <span class="post-cat bg-success color-white"><?= $getdatabyid['diskon'] . '%'; ?></span>
-                                 </div>
-                             </div>
-                         </div>
-                     <?php endif; ?>
-                     <h6>Deskripsi</h6>
-                     <p><?= htmlspecialchars_decode($getdatabyid['deskripsi_event']); ?></p>
                  </div>
              </div>
              <!--comment form-->
-             <div class="col-lg-6 col-md-12">
-                 <!-- <div class="comment-form"> -->
-                 <h3 class="mt-100 mb-30">Pesan Sekarang</h3>
-                 <form class="form-contact comment_form" action="#" id="commentForm">
+             <div class="col-lg-6 col-md-12 mt-100 mb-30">
+                 <div class="row">
+                     <div class="col-2">
+                         <h6>Tanggal Acara</h6>
+                     </div>
+                     <div class="col entry-meta meta-0 font-small mb-30 mt-1">
+                         <span class="post-cat bg-danger color-white"><?= $getdatabyid['tgl_acara']  ?></span>
+                     </div>
+                 </div>
+                 <div class="row">
+                     <div class="col-2">
+                         <h6>Batas Beli</h6>
+                     </div>
+                     <div class="col entry-meta meta-0 font-small mb-30 mt-1">
+                         <span class="post-cat bg-danger color-white"><?= $getdatabyid['tgl_berakhir']  ?></span>
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-2">
+                         <h6>Harga Tiket</h6>
+                     </div>
+                     <div class="col">
+                         <?php
+                            if ($getdatabyid['harga_tiket'] == 0) { ?>
+                             <a class="entry-meta meta-0"><span class="post-in background5 text-dark font-x-small">GRATIS</span></a>
+                         <?php } else if ($getdatabyid['diskon'] != 0) { ?>
+                             <?php $h_diskon = $getdatabyid['harga_tiket'] * ($getdatabyid['diskon'] / 100);
+                                $diskon = $getdatabyid['harga_tiket'] - $h_diskon;
+                                ?>
+                             <div class="entry-meta meta-0 font-small mb-30 mt-1">
+                                 <span class="post-cat bg-success color-white" style="text-decoration: line-through;">Rp <?= number_format($getdatabyid['harga_tiket'], 0, ',', '.') ?></span>
+                                 <span class="post-in background5 text-dark font-x-small">Rp <?= number_format($diskon, 0, ',', '.') ?></span>
+                             </div>
+                         <?php } else { ?>
+                             <div class="entry-meta meta-0 font-small mb-30 mt-1">
+                                 <span class="post-cat bg-success color-white">Rp <?= number_format($getdatabyid['harga_tiket'], 0, ',', '.') ?></span>
+                             </div>
+                         <?php } ?>
+                     </div>
+                 </div>
+                 <?php if ($getdatabyid['diskon'] != 0) : ?>
                      <div class="row">
-                         <div class="col-sm-6">
-                             <div class="form-group">
-                                 <input class="form-control" name="name" id="name" type="text" placeholder="Name">
-                             </div>
+                         <div class="col-2">
+                             <h6>Diskon</h6>
                          </div>
-                         <div class="col-sm-6">
-                             <div class="form-group">
-                                 <input class="form-control" name="email" id="email" type="email" placeholder="Email">
-                             </div>
-                         </div>
-                         <div class="col-12">
-                             <div class="form-group">
-                                 <input class="form-control" name="website" id="website" type="text" placeholder="Website">
-                             </div>
-                         </div>
-                         <div class="col-12">
-                             <div class="form-group">
-                                 <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9" placeholder="Write Comment"></textarea>
+                         <div class="col">
+                             <div class="entry-meta meta-0 font-small mb-30 mt-1">
+                                 <span class="post-cat bg-success color-white"><?= $getdatabyid['diskon'] . '%'; ?></span>
                              </div>
                          </div>
                      </div>
+                 <?php endif; ?>
+                 <h6>Deskripsi</h6>
+                 <p><?= htmlspecialchars_decode($getdatabyid['deskripsi_event']); ?></p>
+                 <div class="row">
                      <div class="form-group">
-                         <button type="submit" class="button button-contactForm">Pesan</button>
+                         <div class="col-sm-5">
+                             <a class="btn btn-primary" href="<?= base_url('home/tambah_keranjang_event/') . $getdatabyid['id_event']; ?>" role="button">Tambah Keranjang</a>
+                         </div>
                      </div>
-                 </form>
-                 <!-- </div> -->
+                     <div class="col-sm-6">
+                         <a class="btn btn-primary" href="<?= base_url('home/event/'); ?>" role="button">Kembali</a>
+                     </div>
+
+                 </div>
              </div>
          </div>
          <!--End row-->
