bf4b69eeb310507b28a250803468b0bc42f80bac
Diaz Adha Asri Prakoso <76459391+diazadha@users.noreply.github.com>
2021-11-27 15:11:05 +0700

    style(event): change css in detail event

--- application/views/detail_event.php
+++ application/views/detail_event.php
@@ -16,13 +16,12 @@
                          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                              <center>
                                  <div class="carousel-inner">
-
                                      <div class="carousel-item active">
-                                         <img class="d-block w-50" src="<?= base_url('assets/uploads/foto_event/') . $getdatabyid['foto'] ?>" alt="First slide" style="height:400px;">
+                                         <img class="d-block w-50" src="<?= base_url('assets/uploads/foto_event/') . $getdatabyid['foto'] ?>" alt="First slide" style="height:310px;">
                                      </div>
                                      <?php foreach ($getfotobyid as $m) : ?>
                                          <div class="carousel-item">
-                                             <img class="d-block w-50" src="<?= base_url('assets/uploads/foto_event/') . $m['foto'] ?>" alt="" style="height: 400px;">
+                                             <img class="d-block w-50" src="<?= base_url('assets/uploads/foto_event/') . $m['foto'] ?>" alt="" style="height: 310px;">
                                          </div>
                                      <?php endforeach; ?>
                                  </div>
@@ -98,11 +97,11 @@
                  <div class="row">
                      <div class="form-group">
                          <div class="col-sm-5">
-                             <a class="btn btn-primary" href="<?= base_url('home/tambah_keranjang_event/') . $getdatabyid['id_event']; ?>" role="button">Tambah Keranjang</a>
+                             <a class="btn btn-primary" href="<?= base_url('home/tambah_keranjang_event/') . $getdatabyid['id_event']; ?>" role="button" style="background-color: #E6505F;">Tambah Keranjang</a>
                          </div>
                      </div>
                      <div class="col-sm-6">
-                         <a class="btn btn-primary" href="<?= base_url('home/event/'); ?>" role="button">Kembali</a>
+                         <a class="btn btn-secondary" href="<?= base_url('home/event/'); ?>" role="button">Kembali</a>
                      </div>
 
                  </div>
