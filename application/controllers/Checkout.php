ebc1e2650d6df9181f544053623e781f45ffb881
Makmudin <id.makmudin@gmail.com>
2021-11-30 00:15:47 +0700

    update

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -96,54 +96,54 @@ class Checkout extends CI_Controller
         $this->db->insert_batch('keranjang_event_peserta', $value);
 
         //send email notif;
-        $config = [
-            'protocol' => 'smtp',
-            'smtp_host' => 'ssl://smtp.googlemail.com',
-            'smtp_user' => 'intrvrt.me1@gmail.com',
-            'smtp_pass' => 'Ayamgoreng123',
-            'smtp_port' => 465,
-            'mailtype' => 'html',
-            'charset' => 'utf-8',
-            'newline' => "\r\n",
-            'validation' => TRUE // bool whether to validate email or not  
-        ];
-        $this->load->library('email', $config);
-        $this->email->initialize($config);
-        $this->email->from('intrvrt.me1@gmail.com', 'Intrvrt.me');
-        $this->email->to($user->email);
-        $this->email->subject('Verifikasi Akun');
+        // $config = [
+        //     'protocol' => 'smtp',
+        //     'smtp_host' => 'ssl://smtp.googlemail.com',
+        //     'smtp_user' => 'intrvrt.me1@gmail.com',
+        //     'smtp_pass' => 'Ayamgoreng123',
+        //     'smtp_port' => 465,
+        //     'mailtype' => 'html',
+        //     'charset' => 'utf-8',
+        //     'newline' => "\r\n",
+        //     'validation' => TRUE // bool whether to validate email or not  
+        // ];
+        // $this->load->library('email', $config);
+        // $this->email->initialize($config);
+        // $this->email->from('intrvrt.me1@gmail.com', 'Intrvrt.me');
+        // $this->email->to($user->email);
+        // $this->email->subject('Verifikasi Akun');
 
-        $acara = $this->Tiket_model->get_acara($id_transaksi)->result();
-        $table="<table>
-        <tr>
-            <th><b>No</b></th>
-            <th><b>Nama Event</b></th>
-            <th><b>Nama Kategori</b></th>
-        </tr>
-        ";
-        $no=1;
-        foreach($acara as $a){
-            $table.="<tr>
-                        <td>".$no."</td>
-                        <td>".$a->nama_event."</td>
-                        <td>".$a->nama_kategori."</td>
-                    </tr>";
-            $no++;
-        }
-        $table.="</table>";
+        // $acara = $this->Tiket_model->get_acara($id_transaksi)->result();
+        // $table="<table>
+        // <tr>
+        //     <th><b>No</b></th>
+        //     <th><b>Nama Event</b></th>
+        //     <th><b>Nama Kategori</b></th>
+        // </tr>
+        // ";
+        // $no=1;
+        // foreach($acara as $a){
+        //     $table.="<tr>
+        //                 <td>".$no."</td>
+        //                 <td>".$a->nama_event."</td>
+        //                 <td>".$a->nama_kategori."</td>
+        //             </tr>";
+        //     $no++;
+        // }
+        // $table.="</table>";
 
-        $message = "Hi <b>$user->nama_user</b>, <br>
-        Pembelian Tiket Berhasil !!!<br>
+        // $message = "Hi <b>$user->nama_user</b>, <br>
+        // Pembelian Tiket Berhasil !!!<br>
         
-        $table
+        // $table
 
-        Terimakasih telah melakukan pembelian tike event. Link acara akan dikirim h-1 acara<br><br>
-        ";
-        $this->email->message($message);
+        // Terimakasih telah melakukan pembelian tike event. Link acara akan dikirim h-1 acara<br><br>
+        // ";
+        // $this->email->message($message);
 
-        if (!$this->email->send()) {
-            echo $this->email->print_debugger();
-        }
+        // if (!$this->email->send()) {
+        //     echo $this->email->print_debugger();
+        // }
 
 
         if($totaltagihan != 0){
@@ -152,7 +152,7 @@ class Checkout extends CI_Controller
                 "external_id" => 'intrvrt.me-'.$id_transaksi,
                 "bank_code" => $vaBank,
                 "name" => $user->nama_user,
-                "expected_amount" => $this->input->post('tagihan'),
+                "expected_amount" => $this->input->post('totaltagihan'),
                 "expiration_date" => date('c', mktime(date('H'), date('i'),date('s'),date('m'),date('d') + 1,date('y'))),
                 "is_single_use" => TRUE
             ];
